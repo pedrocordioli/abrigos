057dffb81786d1e9646df9591025e9095c00a29d
Pedro Cordioli <112526106+pedrocordioli@users.noreply.github.com>
2023-07-14 19:34:17 -0300

    update
    a

--- inc/bloco-abrigos.php
+++ inc/bloco-abrigos.php
@@ -1,64 +1,43 @@
 <div class="container-cards">
     <div class="row p-5">
-
         <div class="card m-auto p-2" style="width: 23rem;">
-            <img src="../img/anjospeludos.png" class="card-img-top" alt="...">
+            <a href="http://anjospeludos.org.br/"><img src="../img/anjospeludos.png" class="card-img-top" alt="..."></a>
             <div class="card-body">
-                <h3 class="card-text">Anjos Peludos<br>Americana-SP</h3>
+            <center><h3 class="card-text">Anjos Peludos<br>Americana-SP</h3></center>
             </div>
         </div>
-
         <div class="card m-auto p-2" style="width: 23rem;">
-
-            <img src="../img/animaistemvoz.png" class="card-img-top" alt="...">
-
+        <a href="https://linktr.ee/animaistemvoz"><img src="../img/animaistemvoz.png" class="card-img-top" alt="..."></a>
             <div class="card-body">
-
-                <h3 class="card-text">Animais Tem Voz<br>Americana-SP</h3>
-
-            </div>
-
-
-        </div>
-        <div class="card m-auto p-2" style="width: 23rem;">
-
-            <img src="../img/vidaanimal.jpg" class="card-img-top" alt="...">
-
-            <div class="card-body">
-
-                <h3 class="card-text">Ong Vida Animal<br>Americana-SP</h3>
-
+            <center><h3 class="card-text">Animais Tem Voz<br>Americana-SP</h3></center>
             </div>
         </div>
-
+        <div class="card m-auto p-2" style="width: 23rem;">
+            <a href="https://www.facebook.com/ongvidaanimalamericana/"><img src="../img/vidaanimal.jpg" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+            <center><h3 class="card-text">Ong Vida Animal<br>Americana-SP</h3></center>
+            </div>
+        </div>
     </div>
-
-
     <div class="row p-5">
         <div class="card m-auto p-2" style="width: 23rem;">
-            <img src="../img/spasb.png" class="card-img-top" alt="...">
+            <a href="https://sosanimaisspasb.com.br/"><img src="../img/spasb.png" class="card-img-top" alt="..."></a>
             <div class="card-body">
-                <h3 class="card-text">SPASB <br> SBO-SP</h3>
+            <center><h3 class="card-text">SPASB <br> SBO-SP</h3></center>
+            </div>
+        </div>
+        <div class="card m-auto p-2" style="width: 23rem;">
+            <a href="http://viralataviravida.org.br/novo/"><img src="../img/viravira.png" class="card-img-top" alt="..."></a>
+            <div class="card-body">
+            <center><h3 class="card-text">Vira Lata Vira Vida <br> Piracicaba-SP</h3></center>
             </div>
         </div>
         <div class="card m-auto p-2" style="width: 23rem;">
 
-            <img src="../img/viravira.png" class="card-img-top" alt="...">
+            <a href="https://aaac.org.br/"><img src="../img/aaac.png" class="card-img-top" alt="..."></a>
 
             <div class="card-body">
-
-                <h3 class="card-text">Vira Lata Vira Vida <br> Piracicaba-SP</h3>
-
-            </div>
-
-
-        </div>
-        <div class="card m-auto p-2" style="width: 23rem;">
-
-            <img src="../img/aaac.png" class="card-img-top" alt="...">
-
-            <div class="card-body">
-                <h3 class="card-text">A.A.A.C <br> Campinas-SP</h3>
+            <center><h3 class="card-text">A.A.A.C <br> Campinas-SP</h3></center>
 
             </div>
         </div>
@@ -66,50 +45,48 @@
 
     <div class="row p-5">
         <div class="card m-auto p-2" style="width: 23rem;">
-            <img src="../img/gaar.png" class="card-img-top" alt="...">
+            <a href="https://gaarcampinas.org/"><img src="../img/gaar.png" class="card-img-top" alt="..."></a>
             <div class="card-body">
-                <h3 class="card-text">GAAR<br> Campinas-SP</h3>
+            <center><h3 class="card-text">GAAR<br> Campinas-SP</h3></center>
             </div>
         </div>
         <div class="card m-auto p-2" style="width: 23rem;">
-            <img src="../img/gavaa.jpg" class="card-img-top" alt="...">
+            <a href="https://www.gavaa.com.br/"><img src="../img/gavaa.jpg" class="card-img-top" alt="..."></a>
             <div class="card-body">
-                <h3 class="card-text">GAVAA<br> Campinas-SP</h3>
+            <center><h3 class="card-text">GAVAA<br> Campinas-SP</h3></center>
             </div>
         </div>
         <div class="card m-auto p-2" style="width: 23rem;">
-            <img src="../img/operacaoresgate.jpg" class="card-img-top" alt="...">
+            <a href="http://operacaoresgatecampinas.com.br/"><img src="../img/operacaoresgate.jpg" class="card-img-top" alt="..."></a>
             <div class="card-body">
-                <h3 class="card-text">Operacão Resgate<br> Campinas-SP</h3>
+            <center><h3 class="card-text">Operacão Resgate<br> Campinas-SP</h3></center>
             </div>
         </div>
     </div>
     <div class="row p-5">
         <div class="card m-auto p-2" style="width: 23rem;">
-            <img src="../img/amordebicho.jpg" class="card-img-top" alt="...">
+            <a href="http://amordebicho.org.br/"><img src="../img/amordebicho.jpg" class="card-img-top" alt="..."></a>
             <div class="card-body">
-                <h3 class="card-text">ONG Amor de Bicho<br> Campinas-SP</h3>
+            <center><h3 class="card-text">ONG Amor de Bicho<br> Campinas-SP</h3></center>
             </div>
         </div>
         <div class="card m-auto p-2" style="width: 23rem;">
 
-            <img src="../img/lati.png" class="card-img-top" alt="...">
+            <a href="https://laticao.org.br/"><img src="../img/lati.png" class="card-img-top" alt="..."></a>
 
             <div class="card-body">
 
-                <h3 class="card-text">Laticão<br> Campinas-SP</h3>
+            <center><h3 class="card-text">Laticão<br> Campinas-SP</h3></center>
 
             </div>
-
-
         </div>
         <div class="card m-auto p-2" style="width: 23rem;">
 
-            <img src="../img/anjosderua.jpg" class="card-img-top" alt="...">
+            <a href="https://www.facebook.com/anjosderuaoficial"><img src="../img/anjosderua.jpg" class="card-img-top" alt="..."></a>
 
             <div class="card-body">
 
-                <h3 class="card-text">Anjos de Rua<br> Campinas-SP</h3>
+                <center><h3 class="card-text">Anjos de Rua<br> Campinas-SP</h3></center>
 
             </div>
         </div>
